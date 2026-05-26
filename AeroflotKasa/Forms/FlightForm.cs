a318064b8f94b976228c340abf9316761d445ab9
denysdanilievskyi-byte <denys.danilievskyi@nure.ua>
2026-05-27 02:42:51 +0300

    Add DateTimePicker, NumericUpDown and buttons to FlightForm

--- AeroflotKasa/Forms/FlightForm.cs
+++ AeroflotKasa/Forms/FlightForm.cs
@@ -7,5 +7,15 @@ namespace AeroflotKasa.Forms;
 /// </summary>
 public class FlightForm : Form
 {
+    private TextBox txtFlightNumber = new();
+    private TextBox txtRoute = new();
+    private TextBox txtStops = new();
+    private TextBox txtDays = new();
+    private DateTimePicker dtpDeparture = new();
+    private NumericUpDown numSeats = new();
+    private Button btnSave = new();
+    private Button btnCancel = new();
+
+
     public Flight? Flight { get; private set; }
 }
