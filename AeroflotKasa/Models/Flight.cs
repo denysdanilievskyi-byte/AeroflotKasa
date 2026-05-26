09def0387626c4e06a96a2afdc074487b824471e
denysdanilievskyi-byte <denys.danilievskyi@nure.ua>
2026-05-26 22:43:58 +0300

    Add AvailableSeats to Flight and Passengers list to Flight to establish 1-to-many relationship

--- AeroflotKasa/Models/Flight.cs
+++ AeroflotKasa/Models/Flight.cs
@@ -7,4 +7,7 @@ public class Flight
     public string IntermediateStops { get; set; } = string.Empty;
     public DateTime DepartureTime { get; set; }
     public string FlightDays { get; set; } = string.Empty;
+    public int AvailableSeats { get; set; }
+
+    public List<Passenger> Passengers { get; set; } = new();
 }
---
+++ AeroflotKasa/Models/Passenger.cs
@@ -0,0 +1,7 @@
+﻿namespace AeroflotKasa.Models;
+
+public class Passenger
+{
+    public string FullName { get; set; } = string.Empty;
+    public string PassportData { get; set; } = string.Empty;
+}
