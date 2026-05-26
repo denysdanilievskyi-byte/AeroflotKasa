9c661cb190a2ea8cbd93832f116e74f8918fb24d
denysdanilievskyi-byte <denys.danilievskyi@nure.ua>
2026-05-27 02:51:51 +0300

    Initialize BookingForm window and display flight info

--- AeroflotKasa/Forms/BookingForm.cs
+++ AeroflotKasa/Forms/BookingForm.cs
@@ -15,4 +15,29 @@ public class BookingForm : Form
 
     public int TicketCount => (int)numTickets.Value;
     public Passenger PassengerInfo { get; private set; } = new Passenger();
+
+    public BookingForm(Flight flight)
+    {
+        InitializeComponent(flight);
+    }
+
+    private void InitializeComponent(Flight flight)
+    {
+        this.Text = "Оформлення квитків";
+        this.Size = new Size(350, 300);
+        this.StartPosition = FormStartPosition.CenterParent;
+        this.FormBorderStyle = FormBorderStyle.FixedDialog;
+        this.MaximizeBox = false;
+        this.MinimizeBox = false;
+        this.KeyPreview = true;
+        this.KeyDown += (s, e) => { if (e.KeyCode == Keys.Escape) this.DialogResult = DialogResult.Cancel; };
+
+        var lblInfo = new Label
+        {
+            Text = $"Рейс: {flight.FlightNumber}\nМаршрут: {flight.Route}\nВільних місць: {flight.AvailableSeats}",
+            Location = new Point(10, 10),
+            AutoSize = true
+        };
+        this.Controls.Add(lblInfo);
+    }
 }
