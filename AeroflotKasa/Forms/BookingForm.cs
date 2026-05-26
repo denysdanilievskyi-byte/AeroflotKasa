522c22656e161fadd6b427abfd157afda4f0fd4c
denysdanilievskyi-byte <denys.danilievskyi@nure.ua>
2026-05-27 02:49:54 +0300

    Create BookingForm base structure and properties

---
+++ AeroflotKasa/Forms/BookingForm.cs
@@ -0,0 +1,18 @@
+﻿using AeroflotKasa.Models;
+
+namespace AeroflotKasa.Forms;
+
+/// <summary>
+/// Форма для оформлення квитків на рейс та узгодження з пасажиром.
+/// </summary>
+public class BookingForm : Form
+{
+    private TextBox txtFullName = new();
+    private TextBox txtPassportData = new();
+    private NumericUpDown numTickets = new();
+    private Button btnConfirm = new();
+    private Button btnCancel = new();
+
+    public int TicketCount => (int)numTickets.Value;
+    public Passenger PassengerInfo { get; private set; } = new Passenger();
+}
