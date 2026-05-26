77daf40b4e2a0c14b96cab4f937934b0609df995
denysdanilievskyi-byte <denys.danilievskyi@nure.ua>
2026-05-27 00:15:14 +0300

    Set HasUnsavedChanges flag on CRUD operations

--- AeroflotKasa/Services/FlightService.cs
+++ AeroflotKasa/Services/FlightService.cs
@@ -8,11 +8,13 @@ public class FlightService
     private readonly string _filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "flights.json");
     private List<Flight> _flights = new();
 
+    public bool HasUnsavedChanges { get; private set; } = false;
     public IReadOnlyList<Flight> GetAllFlights() => _flights;
 
     public void AddFlight(Flight flight)
     {
         _flights.Add(flight);
+        HasUnsavedChanges = true;
     }
 
     public void UpdateFlight(Flight oldFlight, Flight newFlight)
@@ -21,11 +23,13 @@ public class FlightService
         if (index >= 0)
         {
             _flights[index] = newFlight;
+            HasUnsavedChanges = true;
         }
     }
 
     public void DeleteFlight(Flight flight)
     {
         _flights.Remove(flight);
+        HasUnsavedChanges = true;
     }
 }
