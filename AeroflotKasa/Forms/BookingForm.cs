6af327e3529342622d1c716dd50b4df8d4c46d5b
denysdanilievskyi-byte <denys.danilievskyi@nure.ua>
2026-05-27 02:52:35 +0300

    Add passenger info and ticket count inputs to BookingForm

--- AeroflotKasa/Forms/BookingForm.cs
+++ AeroflotKasa/Forms/BookingForm.cs
@@ -39,5 +39,29 @@ public class BookingForm : Form
             AutoSize = true
         };
         this.Controls.Add(lblInfo);
+
+        var lblFullName = new Label { Text = "ПІБ Пасажира:", Location = new Point(10, 75), AutoSize = true };
+        txtFullName.Location = new Point(130, 70);
+        txtFullName.Size = new Size(180, 25);
+        txtFullName.TabIndex = 0;
+
+        var lblPassport = new Label { Text = "Дані паспорта:", Location = new Point(10, 105), AutoSize = true };
+        txtPassportData.Location = new Point(130, 100);
+        txtPassportData.Size = new Size(180, 25);
+        txtPassportData.TabIndex = 1;
+
+        var lblTickets = new Label { Text = "Кількість квитків:", Location = new Point(10, 135), AutoSize = true };
+        numTickets.Location = new Point(130, 130);
+        numTickets.Size = new Size(180, 25);
+        numTickets.Minimum = 1;
+        numTickets.Maximum = flight.AvailableSeats > 0 ? flight.AvailableSeats : 1;
+        numTickets.TabIndex = 2;
+
+        this.Controls.Add(lblFullName);
+        this.Controls.Add(txtFullName);
+        this.Controls.Add(lblPassport);
+        this.Controls.Add(txtPassportData);
+        this.Controls.Add(lblTickets);
+        this.Controls.Add(numTickets);
     }
 }
